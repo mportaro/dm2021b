d55bb330a97f1a0a5728d76e3d60933297521ea8
Portaro <marcos.portaro@accenture.com>
2021-09-26 17:33:30 -0300

    Solo seed propia

--- src/lightgbm/712_lgb_bin2_lagdelta.r
+++ src/lightgbm/712_lgb_bin2_lagdelta.r
@@ -32,7 +32,7 @@ require("mlrMBO")
 #para poder usarlo en la PC y en la nube sin tener que cambiar la ruta
 #cambiar aqui las rutas en su maquina
 switch ( Sys.info()[['sysname']],
-         Windows = { directory.root  <-  "M:\\" },   #Windows
+         Windows = { directory.root  <-  "C:/Users/marcos.portaro/Google Drive/Data.Science/ITBA/05-Data.Mining/dm2021b/" },   #Windows
          Darwin  = { directory.root  <-  "~/dm/" },  #Apple MAC
          Linux   = { directory.root  <-  "~/buckets/b1/" } #Google Cloud
        )
@@ -65,7 +65,7 @@ hs <- makeParamSet(
 
 campos_malos  <- c( )   #Intencionalmente VACIO
 
-ksemilla_azar  <- 102191  #Aqui poner la propia semilla
+ksemilla_azar  <- 320009  #Aqui poner la propia semilla
 #------------------------------------------------------------------------------
 #Funcion que lleva el registro de los experimentos
 
