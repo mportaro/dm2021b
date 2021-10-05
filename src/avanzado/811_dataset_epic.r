2a626909fd5ee74f6a64b817b0ce5b844cd107a5
Portaro <marcos.portaro@accenture.com>
2021-10-05 11:29:17 -0300

    v003

--- src/avanzado/811_dataset_epic.r
+++ src/avanzado/811_dataset_epic.r
@@ -22,9 +22,10 @@ setwd( directory.root )
 
 palancas  <- list()  #variable con las palancas para activar/desactivar
 
-palancas$version  <- "v002"   #Muy importante, ir cambiando la version
+palancas$version  <- "v003"   #Muy importante, ir cambiando la version
 
-palancas$variablesdrift  <- c()   #aqui van las columnas que se quieren eliminar
+palancas$variablesdrift  <- c("mcuenta_debitos_automaticos", "cpagomiscuentas", 
+                   "mpagomiscuentas", "Master_mfinanciacion_limite")   #aqui van las columnas que se quieren eliminar
 
 palancas$corregir <-  TRUE    # TRUE o FALSE
 
@@ -33,10 +34,10 @@ palancas$nuevasvars <-  TRUE  #si quiero hacer Feature Engineering manual
 palancas$dummiesNA  <-  FALSE #La idea de Santiago Dellachiesa
 
 palancas$lag1   <- TRUE    #lag de orden 1
-palancas$delta1 <- TRUE    # campo -  lag de orden 1 
-palancas$lag2   <- FALSE
+palancas$delta1 <- FALSE    # campo -  lag de orden 1 
+palancas$lag2   <- TRUE
 palancas$delta2 <- FALSE
-palancas$lag3   <- FALSE
+palancas$lag3   <- TRUE
 palancas$delta3 <- FALSE
 palancas$lag4   <- FALSE
 palancas$delta4 <- FALSE
@@ -109,107 +110,148 @@ Corregir  <- function( dataset )
 {
   #acomodo los errores del dataset
 
-  dataset[ foto_mes==201801,  internet   := NA ]
-  dataset[ foto_mes==201801,  thomebanking   := NA ]
-  dataset[ foto_mes==201801,  chomebanking_transacciones   := NA ]
-  dataset[ foto_mes==201801,  tcallcenter   := NA ]
-  dataset[ foto_mes==201801,  ccallcenter_transacciones   := NA ]
-  dataset[ foto_mes==201801,  cprestamos_personales   := NA ]
-  dataset[ foto_mes==201801,  mprestamos_personales   := NA ]
-  dataset[ foto_mes==201801,  mprestamos_hipotecarios  := NA ]
-  dataset[ foto_mes==201801,  ccajas_transacciones   := NA ]
-  dataset[ foto_mes==201801,  ccajas_consultas   := NA ]
-  dataset[ foto_mes==201801,  ccajas_depositos   := NA ]
-  dataset[ foto_mes==201801,  ccajas_extracciones   := NA ]
-  dataset[ foto_mes==201801,  ccajas_otras   := NA ]
+dataset[ foto_mes==201801,  internet   := NA ]
+dataset[ foto_mes==201801,  thomebanking   := NA ]
+dataset[ foto_mes==201801,  chomebanking_transacciones   := NA ]
+dataset[ foto_mes==201801,  tcallcenter   := NA ]
+dataset[ foto_mes==201801,  ccallcenter_transacciones   := NA ]
+dataset[ foto_mes==201801,  cprestamos_personales   := NA ]
+dataset[ foto_mes==201801,  mprestamos_personales   := NA ]
+dataset[ foto_mes==201801,  mprestamos_hipotecarios  := NA ]
+dataset[ foto_mes==201801,  ccajas_transacciones   := NA ]
+dataset[ foto_mes==201801,  ccajas_consultas   := NA ]
+dataset[ foto_mes==201801,  ccajas_depositos   := NA ]
+dataset[ foto_mes==201801,  ccajas_extracciones   := NA ]
+dataset[ foto_mes==201801,  ccajas_otras   := NA ]
 
-  dataset[ foto_mes==201806,  tcallcenter   :=  NA ]
-  dataset[ foto_mes==201806,  ccallcenter_transacciones   :=  NA ]
+dataset[ foto_mes==201806,  tcallcenter   :=  NA ]
+dataset[ foto_mes==201806,  ccallcenter_transacciones   :=  NA ]
 
-  dataset[ foto_mes==201904,  ctarjeta_visa_debitos_automaticos  :=  NA ]
-  dataset[ foto_mes==201904,  mttarjeta_visa_debitos_automaticos := NA ]
-  dataset[ foto_mes==201904,  Visa_mfinanciacion_limite := NA ]
+dataset[ foto_mes==201904,  ctarjeta_visa_debitos_automaticos  :=  NA ]
+dataset[ foto_mes==201904,  mtarjeta_visa_debitos_automaticos := NA ]
+dataset[ foto_mes==202006,  Visa_mfinanciacion_limite := NA ]
 
-  dataset[ foto_mes==201905,  mrentabilidad     := NA ]
-  dataset[ foto_mes==201905,  mrentabilidad_annual     := NA ]
-  dataset[ foto_mes==201905,  mcomisiones      := NA ]
-  dataset[ foto_mes==201905,  mpasivos_margen  := NA ]
-  dataset[ foto_mes==201905,  mactivos_margen  := NA ]
-  dataset[ foto_mes==201905,  ctarjeta_visa_debitos_automaticos  := NA ]
-  dataset[ foto_mes==201905,  ccomisiones_otras := NA ]
-  dataset[ foto_mes==201905,  mcomisiones_otras := NA ]
+dataset[ foto_mes==201905,  mrentabilidad     := NA ]
+dataset[ foto_mes==201905,  mrentabilidad_annual     := NA ]
+dataset[ foto_mes==201905,  mcomisiones      := NA ]
+dataset[ foto_mes==201905,  mpasivos_margen  := NA ]
+dataset[ foto_mes==201905,  mactivos_margen  := NA ]
+dataset[ foto_mes==201905,  ccomisiones_otras := NA ]
+dataset[ foto_mes==201905,  mcomisiones_otras := NA ]
 
-  dataset[ foto_mes==201910,  mpasivos_margen   := NA ]
-  dataset[ foto_mes==201910,  mactivos_margen   := NA ]
-  dataset[ foto_mes==201910,  ccomisiones_otras := NA ]
-  dataset[ foto_mes==201910,  mcomisiones_otras := NA ]
-  dataset[ foto_mes==201910,  mcomisiones       := NA ]
-  dataset[ foto_mes==201910,  mrentabilidad     := NA ]
-  dataset[ foto_mes==201910,  mrentabilidad_annual        := NA ]
-  dataset[ foto_mes==201910,  chomebanking_transacciones  := NA ]
-  dataset[ foto_mes==201910,  ctarjeta_visa_descuentos    := NA ]
-  dataset[ foto_mes==201910,  ctarjeta_master_descuentos  := NA ]
-  dataset[ foto_mes==201910,  mtarjeta_visa_descuentos    := NA ]
-  dataset[ foto_mes==201910,  mtarjeta_master_descuentos  := NA ]
-  dataset[ foto_mes==201910,  ccajeros_propios_descuentos := NA ]
-  dataset[ foto_mes==201910,  mcajeros_propios_descuentos := NA ]
+dataset[ foto_mes==201910,  mpasivos_margen   := NA ]
+dataset[ foto_mes==201910,  mactivos_margen   := NA ]
+dataset[ foto_mes==201910,  ccomisiones_otras := NA ]
+dataset[ foto_mes==201910,  mcomisiones_otras := NA ]
+dataset[ foto_mes==201910,  mcomisiones       := NA ]
+dataset[ foto_mes==201910,  mrentabilidad     := NA ]
+dataset[ foto_mes==201910,  mrentabilidad_annual        := NA ]
+dataset[ foto_mes==201910,  chomebanking_transacciones  := NA ]
+dataset[ foto_mes==201910,  ctarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==201910,  ctarjeta_master_descuentos  := NA ]
+dataset[ foto_mes==201910,  mtarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==201910,  mtarjeta_master_descuentos  := NA ]
+dataset[ foto_mes==201910,  ccajeros_propios_descuentos := NA ]
+dataset[ foto_mes==201910,  mcajeros_propios_descuentos := NA ]
 
-  dataset[ foto_mes==202001,  cliente_vip   := NA ]
+dataset[ foto_mes==202001,  cliente_vip   := NA ]
+dataset[ foto_mes==202001,  ctarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==202001,  mtarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==202001,  ctarjeta_master_descuentos    := NA ]
+dataset[ foto_mes==202001,  mtarjeta_master_descuentos    := NA ]
 
-  dataset[ foto_mes==202006,  active_quarter   := NA ]
-  dataset[ foto_mes==202006,  internet   := NA ]
-  dataset[ foto_mes==202006,  mrentabilidad   := NA ]
-  dataset[ foto_mes==202006,  mrentabilidad_annual   := NA ]
-  dataset[ foto_mes==202006,  mcomisiones   := NA ]
-  dataset[ foto_mes==202006,  mactivos_margen   := NA ]
-  dataset[ foto_mes==202006,  mpasivos_margen   := NA ]
-  dataset[ foto_mes==202006,  mcuentas_saldo   := NA ]
-  dataset[ foto_mes==202006,  ctarjeta_debito_transacciones   := NA ]
-  dataset[ foto_mes==202006,  mautoservicio   := NA ]
-  dataset[ foto_mes==202006,  ctarjeta_visa_transacciones   := NA ]
-  dataset[ foto_mes==202006,  mtarjeta_visa_consumo   := NA ]
-  dataset[ foto_mes==202006,  ctarjeta_master_transacciones   := NA ]
-  dataset[ foto_mes==202006,  mtarjeta_master_consumo   := NA ]
-  dataset[ foto_mes==202006,  ccomisiones_otras   := NA ]
-  dataset[ foto_mes==202006,  mcomisiones_otras   := NA ]
-  dataset[ foto_mes==202006,  cextraccion_autoservicio   := NA ]
-  dataset[ foto_mes==202006,  mextraccion_autoservicio   := NA ]
-  dataset[ foto_mes==202006,  ccheques_depositados   := NA ]
-  dataset[ foto_mes==202006,  mcheques_depositados   := NA ]
-  dataset[ foto_mes==202006,  ccheques_emitidos   := NA ]
-  dataset[ foto_mes==202006,  mcheques_emitidos   := NA ]
-  dataset[ foto_mes==202006,  ccheques_depositados_rechazados   := NA ]
-  dataset[ foto_mes==202006,  mcheques_depositados_rechazados   := NA ]
-  dataset[ foto_mes==202006,  ccheques_emitidos_rechazados   := NA ]
-  dataset[ foto_mes==202006,  mcheques_emitidos_rechazados   := NA ]
-  dataset[ foto_mes==202006,  tcallcenter   := NA ]
-  dataset[ foto_mes==202006,  ccallcenter_transacciones   := NA ]
-  dataset[ foto_mes==202006,  thomebanking   := NA ]
-  dataset[ foto_mes==202006,  chomebanking_transacciones   := NA ]
-  dataset[ foto_mes==202006,  ccajas_transacciones   := NA ]
-  dataset[ foto_mes==202006,  ccajas_consultas   := NA ]
-  dataset[ foto_mes==202006,  ccajas_depositos   := NA ]
-  dataset[ foto_mes==202006,  ccajas_extracciones   := NA ]
-  dataset[ foto_mes==202006,  ccajas_otras   := NA ]
-  dataset[ foto_mes==202006,  catm_trx   := NA ]
-  dataset[ foto_mes==202006,  matm   := NA ]
-  dataset[ foto_mes==202006,  catm_trx_other   := NA ]
-  dataset[ foto_mes==202006,  matm_other   := NA ]
-  dataset[ foto_mes==202006,  ctrx_quarter   := NA ]
-  dataset[ foto_mes==202006,  tmobile_app   := NA ]
-  dataset[ foto_mes==202006,  cmobile_app_trx   := NA ]
+dataset[ foto_mes==202002,  ccajeros_propios_descuentos := NA ]
+dataset[ foto_mes==202002,  mcajeros_propios_descuentos := NA ]
 
+dataset[ foto_mes==202006,  ctarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==202006,  mtarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==202006,  ctarjeta_master_descuentos    := NA ]
+dataset[ foto_mes==202006,  mtarjeta_master_descuentos    := NA ]
+dataset[ foto_mes==202006,  active_quarter   := NA ]
+dataset[ foto_mes==202006,  internet   := NA ]
+dataset[ foto_mes==202006,  mrentabilidad   := NA ]
+dataset[ foto_mes==202006,  mrentabilidad_annual   := NA ]
+dataset[ foto_mes==202006,  mcomisiones   := NA ]
+dataset[ foto_mes==202006,  mactivos_margen   := NA ]
+dataset[ foto_mes==202006,  mpasivos_margen   := NA ]
+dataset[ foto_mes==202006,  mcuentas_saldo   := NA ]
+dataset[ foto_mes==202006,  ctarjeta_debito_transacciones   := NA ]
+dataset[ foto_mes==202006,  mautoservicio   := NA ]
+dataset[ foto_mes==202006,  ctarjeta_visa_transacciones   := NA ]
+dataset[ foto_mes==202006,  mtarjeta_visa_consumo   := NA ]
+dataset[ foto_mes==202006,  ctarjeta_master_transacciones   := NA ]
+dataset[ foto_mes==202006,  mtarjeta_master_consumo   := NA ]
+dataset[ foto_mes==202006,  ccomisiones_otras   := NA ]
+dataset[ foto_mes==202006,  mcomisiones_otras   := NA ]
+dataset[ foto_mes==202006,  cextraccion_autoservicio   := NA ]
+dataset[ foto_mes==202006,  mextraccion_autoservicio   := NA ]
+dataset[ foto_mes==202006,  ccheques_depositados   := NA ]
+dataset[ foto_mes==202006,  mcheques_depositados   := NA ]
+dataset[ foto_mes==202006,  ccheques_emitidos   := NA ]
+dataset[ foto_mes==202006,  mcheques_emitidos   := NA ]
+dataset[ foto_mes==202006,  ccheques_depositados_rechazados   := NA ]
+dataset[ foto_mes==202006,  mcheques_depositados_rechazados   := NA ]
+dataset[ foto_mes==202006,  ccheques_emitidos_rechazados   := NA ]
+dataset[ foto_mes==202006,  mcheques_emitidos_rechazados   := NA ]
+dataset[ foto_mes==202006,  tcallcenter   := NA ]
+dataset[ foto_mes==202006,  ccallcenter_transacciones   := NA ]
+dataset[ foto_mes==202006,  thomebanking   := NA ]
+dataset[ foto_mes==202006,  chomebanking_transacciones   := NA ]
+dataset[ foto_mes==202006,  ccajas_transacciones   := NA ]
+dataset[ foto_mes==202006,  ccajas_consultas   := NA ]
+dataset[ foto_mes==202006,  ccajas_depositos   := NA ]
+dataset[ foto_mes==202006,  ccajas_extracciones   := NA ]
+dataset[ foto_mes==202006,  ccajas_otras   := NA ]
+dataset[ foto_mes==202006,  catm_trx   := NA ]
+dataset[ foto_mes==202006,  matm   := NA ]
+dataset[ foto_mes==202006,  catm_trx_other   := NA ]
+dataset[ foto_mes==202006,  matm_other   := NA ]
+dataset[ foto_mes==202006,  ctrx_quarter   := NA ]
+dataset[ foto_mes==202006,  tmobile_app   := NA ]
+dataset[ foto_mes==202006,  cmobile_app_trx   := NA ]
+dataset[ foto_mes==202006,  ccajeros_propios_descuentos := NA ]
+dataset[ foto_mes==202006,  mcajeros_propios_descuentos := NA ]
 
-  dataset[ foto_mes==202010,  internet  := NA ]
-  dataset[ foto_mes==202011,  internet  := NA ]
-  dataset[ foto_mes==202012,  internet  := NA ]
-  dataset[ foto_mes==202101,  internet  := NA ]
+dataset[ foto_mes==202009,  ctarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==202010,  ctarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==202009,  mtarjeta_visa_descuentos    := NA ]
+dataset[ foto_mes==202010,  mtarjeta_visa_descuentos    := NA ]
 
-  dataset[ foto_mes==202009,  tmobile_app  := NA ]
-  dataset[ foto_mes==202010,  tmobile_app  := NA ]
-  dataset[ foto_mes==202011,  tmobile_app  := NA ]
-  dataset[ foto_mes==202012,  tmobile_app  := NA ]
-  dataset[ foto_mes==202101,  tmobile_app  := NA ]
+dataset[ foto_mes==202009,  ctarjeta_master_descuentos    := NA ]
+dataset[ foto_mes==202010,  ctarjeta_master_descuentos    := NA ]
+dataset[ foto_mes==202009,  mtarjeta_master_descuentos    := NA ]
+dataset[ foto_mes==202010,  mtarjeta_master_descuentos    := NA ]
+
+dataset[ foto_mes==202009,  ccajeros_propios_descuentos := NA ]
+dataset[ foto_mes==202010,  mcajeros_propios_descuentos := NA ]
+
+dataset[ foto_mes==202010,  ccajeros_propios_descuentos := NA ]
+dataset[ foto_mes==202009,  mcajeros_propios_descuentos := NA ]
+
+dataset[ foto_mes==202010,  internet  := NA ]
+dataset[ foto_mes==202011,  internet  := NA ]
+dataset[ foto_mes==202012,  internet  := NA ]
+dataset[ foto_mes==202101,  internet  := NA ]
+dataset[ foto_mes>=201801 & foto_mes<=202101,  internet  := NA ] #ELIMINADO
+
+dataset[ foto_mes==202006,  tmobile_app  := NA ]
+dataset[ foto_mes==202010,  tmobile_app  := NA ]
+dataset[ foto_mes==202011,  tmobile_app  := NA ]
+dataset[ foto_mes==202012,  tmobile_app  := NA ]
+dataset[ foto_mes==202101,  tmobile_app  := NA ]
+
+dataset[ foto_mes>=201801 & foto_mes<=201905,  ctransferencias_recibidas  := NA ]
+dataset[ foto_mes>=201801 & foto_mes<=201905,  mtransferencias_recibidas  := NA ]
+
+dataset[ foto_mes==201802,  Master_fultimo_cierre  := NA ]
+dataset[ foto_mes==201810,  Master_fultimo_cierre  := NA ]
+dataset[ foto_mes==201907,  Master_fultimo_cierre  := NA ]
+dataset[ foto_mes==202009,  Master_fultimo_cierre  := NA ]
+
+dataset[ foto_mes==201802,  Visa_fultimo_cierre  := NA ]
+dataset[ foto_mes==201810,  Visa_fultimo_cierre  := NA ]
+dataset[ foto_mes==201907,  Visa_fultimo_cierre  := NA ]
+dataset[ foto_mes==202009,  Visa_fultimo_cierre  := NA ]
 
   ReportarCampos( dataset )
 }
@@ -276,6 +318,43 @@ AgregarVariables  <- function( dataset )
   dataset[ , mvr_mpagosdolares       := mv_mpagosdolares / mv_mlimitecompra ]
   dataset[ , mvr_mconsumototal       := mv_mconsumototal  / mv_mlimitecompra ]
   dataset[ , mvr_mpagominimo         := mv_mpagominimo  / mv_mlimitecompra ]
+
+# NEW FEATURES --------------------------
+  dataset[ , new_cpayroll := cpayroll_trx > 0] # Aporte del empleador TRUE/FALSE
+  dataset[ , new_cuenta_debitos_automaticos := mcuenta_debitos_automaticos/ccuenta_debitos_automaticos] # Monto Debitos automaticos por transaccion
+  dataset[ , new_tarjeta_visa_debitos_automaticos := ctarjeta_visa_debitos_automaticos > 0] # Debitos automaticos en Visa TRUE/FALSE 
+  dataset[ , new_SaldoBajo := ifelse(mcaja_ahorro < (sum(mcaja_ahorro)/length(mcaja_ahorro))*.05, 1, 0)] # Clientes con saldo promedio bajo
+  dataset[ , new_tenencia := rowSums(cbind(mcuenta_corriente, mcuenta_corriente_adicional, mcaja_ahorro, mcaja_ahorro_adicional), 
+					na.rm=TRUE )] # Tenencia caja de ahorro y cuenta corriente en pesos
+  dataset[ , new_CuentasMVSaldo := mcuentas_saldo/mv_msaldototal] # Relacion Saldos cuentas y tarjetas
+  dataset[ , new_VisaLimiteConsumidoPesos := Visa_mconsumospesos / Visa_mlimitecompra]
+  dataset[ , new_MasterLimiteConsumidoPesos := Master_mconsumospesos / Master_mlimitecompra] 
+  dataset[ , new_DebitosPorTransaccion := mautoservicio/ctarjeta_debito_transacciones]
+  dataset[ , new_mPrestamosTotal := rowSums( cbind( mprestamos_personales, mprestamos_prendarios, mprestamos_hipotecarios) , na.rm=TRUE ) ]
+  dataset[ , new_cPrestamosTotal := rowSums( cbind( cprestamos_personales, cprestamos_prendarios, cprestamos_hipotecarios) , na.rm=TRUE ) ]
+  dataset[ , new_PrestamosUnidad := new_mPrestamosTotal/new_cPrestamosTotal ]
+  dataset[ , new_SaldovsComision := mcuentas_saldo/mcomisiones_mantenimiento] # Relacion Saldos cuentas y comisiones
+  dataset[ , new_MOvCajerosPropvsNoProp := (matm/catm_trx)/(matm_other/catm_trx_other)] # Relacion Saldos y cantidad cajeros propios y no propios
+  dataset[ , new_Visa_limiteConsumido := Visa_mconsumototal / Visa_mlimitecompra]
+  dataset[ , new_Master_limiteConsumido := Master_mconsumototal / Master_mlimitecompra]
+  dataset[ , new_Saldo_MVlimitecompra := mcuentas_saldo/mv_mlimitecompra] # Relacion Saldo en Cuentas y Limite de compra tarjetas
+  dataset[ , new_VisaPagosDiasAlta := Visa_mpagospesos/Visa_fechaalta]
+  dataset[ , new_mactivosZscore := scale(mactivos_margen, center = TRUE, scale = TRUE)] #Normalizacion
+  dataset[ , new_mpasivosZscore := scale(mpasivos_margen, center = TRUE, scale = TRUE)] #Normalizacion
+  dataset[ , new_cliente_antiguedadZscore := scale(cliente_antiguedad, center = TRUE, scale = TRUE)] #Normalizacion
+  dataset[ , new_mtarjeta_visa_consumoZscore := scale(mtarjeta_visa_consumo, center = TRUE, scale = TRUE)] #Normalizacion
+  dataset[ , new_mcaja_ahorroZscore := scale(mcaja_ahorro, center = TRUE, scale = TRUE)] #Normalizacion
+  dataset[ , new_ctrx_quarterLog:= log(ctrx_quarter+1)] #Log Transformation
+  dataset[ , new_Visa_mfinanciacion_limiteLog:= log(Visa_mfinanciacion_limite+1)] #Log Transformation
+  dataset[ , new_Visa_msaldopesosLog:= log(Visa_msaldopesos+1)] #Log Transformation  
+  dataset[ , new_Visa_msaldototalLog:= log(Visa_msaldototal+1)] #Log Transformation  
+  dataset[ , new_Visa_mlimitecompraLog:= log(Visa_mlimitecompra+1)] #Log Transformation  
+  dataset[ , new_Visa_mpagospesosLog:= log(Visa_mpagospesos+1)] #Log Transformation  
+  dataset[ , new_Visa_cconsumosLog:= log(Visa_cconsumos+1)] #Log Transformation
+  dataset[ , new_Master_status_Factor:= as.factor(Master_status)] #Factor conversion
+  dataset[ , new_thomebanking_Factor:= as.factor(thomebanking)] #Factor conversion
+
+# --------------------------------
 
   #valvula de seguridad para evitar valores infinitos
   #paso los infinitos a NULOS
--- src/avanzado/822_epic.r
+++ src/avanzado/822_epic.r
@@ -49,7 +49,7 @@ kexperimento  <- NA   #NA si se corre la primera vez, un valor concreto si es pa
 
 kscript         <- "822_epic"
 
-karch_dataset    <- "./datasets/dataset_epic_v002.csv.gz"
+karch_dataset    <- "./datasets/dataset_epic_v003.csv.gz"
 
 kapply_mes       <- c(202101)  #El mes donde debo aplicar el modelo
 
@@ -78,7 +78,7 @@ hs <- makeParamSet(
 
 campos_malos  <- c()   #aqui se deben cargar todos los campos culpables del Data Drifting
 
-ksemilla_azar  <- 102191  #Aqui poner la propia semilla
+ksemilla_azar  <- 320009  #Aqui poner la propia semilla
 #------------------------------------------------------------------------------
 #Funcion que lleva el registro de los experimentos
 
